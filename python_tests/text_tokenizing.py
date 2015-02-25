da97632e4f2e45a3da22a40590ae8a4da1fbcb88
jarpi <ebls123@gmail.com>
2015-02-25 11:11:23 +0100

    - Improved tfidf

--- python_tests/text_tokenizing.py
+++ python_tests/text_tokenizing.py
@@ -1,55 +1,48 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python 
-import wordHit, docHit, funcs
+import math, wordHit, docHit, funcs
 
-words = {} 
 docs = {} 
+totalWordsInDoc = {}
 
-def countWordInDocs(word): 
-	wordInDocs = 0
+def tf(ocurrences,totalWordsInDoc): 
+	return float(ocurrences)/totalWordsInDoc
+def idf(totalDocs,docsContainingWord): 
+	return round(float(1+math.log(float(totalDocs)/(float(docsContainingWord)),10)),4)
+
+def getDocsContainingWord(word): 
+	wordInDocs = 0 
 	for docKey, docValue in docs.items(): 
-		if docValue.existsWordInContainer(word): 
+		if docValue.has_key(word): 
 			wordInDocs += 1 
 	return wordInDocs 
-
-def countWordsInLine(line): 
-	line.replace("\n","")
-	line.replace("\r\n","") 
-	line.replace(".","") 
-	return len(line.split(None)) 
-
-def parseLines(document): 
-	wordCount = 0 
-	for line in document.docHandler: 
-		wordCount += countWordsInLine(line) 
-		fillWords(line, document) 
-	return wordCount 
-
-def fillWords(line,document): 
-	global words 
-	for word in line.lower().replace('\n',' ').replace('\r','').replace('.','').split(" "): 
-		newHit = document.getWordFromContainer(word) 
-		newHit.addOcurrence() 
-		document.addWordToContainer(newHit) 
+def getDocTotalWords(docKey): 
+	return totalWordsInDoc[docKey] 
 
 if __name__ == '__main__':
-	#documentList = ["./texts/shak.txt","./texts/text2.txt"]; 
-	documentList = ["./texts/test.txt","./texts/test2.txt"]; 
-	wordCount = 0 
+	documentList = ["./texts/t11.txt","./texts/t22.txt"]; 
 	totalDocs = len(documentList) 
-	helpers = funcs.funcs() 
+	# Parse documents 
 	for documentFilename in documentList: 
-		doc = docHit.docHit(documentFilename) 
-		wordCount = parseLines(doc) 
-		doc.setWordCount(wordCount) 
-		docs[documentFilename] = doc 
-		doc.closeDocument() 
-	for docKey, docValue in docs.items(): 
-		for wordKey, wordValue in docValue.getWordContainer(): 
-			wordValue.setTFIDF(helpers.tfidf(wordValue.getOcurrences(), docValue.getWordCount(), totalDocs, countWordInDocs(wordValue.getName())))
-			print wordValue 
-
-
-
-
-
+		docs[documentFilename] = {} 
+		doc = open(documentFilename,'r')  
+		for line in doc: 
+			i = 0
+			for word in line.lower().replace('\n',' ').replace('\r','').replace('.','').replace('"','').split(' '): 
+				if (word != ""): 
+					docs[documentFilename][word] = docs[documentFilename].get(word,0) + 1 
+					i+=1 
+			totalWordsInDoc[documentFilename] = i 
+		doc.close() 
+	# Calc tfidf 
+	for docKey, docWordList in docs.items(): 
+		for wordKey, wordValue in docWordList.items(): 
+			#print "WORDKEY: {} WORDVALUE: {} TOTALWORDS: {} TOTALDOCS: {} WORDAPPEARSIN: {}".format(wordKey,wordValue, getDocTotalWords(docKey),len(docs),getDocsContainingWord(wordKey)) 
+			a = tf(wordValue, getDocTotalWords(docKey)) 
+			b = idf(len(docs),getDocsContainingWord(wordKey)) 
+			docs[docKey][wordKey] = round(a*b, 5) 
+	# Print most relevant words 
+	for docKey, docWordList in docs.items(): 
+		print docKey 
+		for w in sorted(docs[docKey], key=docs[docKey].get, reverse=True): 
+  			print "Word: {} Value: {}".format(w, docs[docKey][w]) 
