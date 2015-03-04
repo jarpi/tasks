482448f226fd7137c4decb058c4e59c913809f87
jarpi <josep.arpi@gmail.com>
2015-03-04 13:25:09 +0100

    - Load settings from json file

--- javascript_tests/jsRunner.js
+++ javascript_tests/jsRunner.js
@@ -1,11 +1,34 @@
 // Javascript runner 
 // Read a file containing a function and eval() it 
 
-var fs = require('fs');  
-var fileName = process.argv[2]; 
-var fileData = ""; 
-fs.readFile(fileName,'utf-8', function(err,data) {
-	if (err) 
-		return err; 
-	eval(data); 
-}); 
+var fs = require('fs'); 
+var path = require('path'); 
+var fileNameRoutesToListen = "routes.json"; 
+var customFileNameFunctions = process.argv[2]; 
+
+function loadCustomFunctions() {
+	fs.readFile(fileName,'utf-8', function(err,data) {
+		if (err) 
+			return err; 
+		eval(data); 
+	}); 
+}
+
+function loadRoutes() {
+	fs.readFile(path.resolve(__dirname, fileNameRoutesToListen), 'utf-8', function(err,data){
+		if (err) 
+			console.dir(err); 
+		// Parse JSON (get json as object)
+		var settings = JSON.parse(data); 
+		// Check for functions list, file, http, github ... 
+		settings.routes.forEach(function(elem){
+			console.log(elem.endpoint); 
+		}); 
+		// Listen on http for each endpoint 
+		// ... 
+	}); 
+} 
+
+loadRoutes(); 
+
+
